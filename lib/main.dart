40a8c2837553c2b709bb9a1737d6f06794cde7f6
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2024-01-11 21:27:36 +0700

    add firestore integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:hahu_hoheng_fitfun/bindings/home_binding.dart';
 import 'package:hahu_hoheng_fitfun/bindings/login_binding.dart';
 import 'package:hahu_hoheng_fitfun/firebase_options.dart';
 import 'package:hahu_hoheng_fitfun/themes/app_theme.dart';
+import 'package:hahu_hoheng_fitfun/views/home.dart';
 import 'package:hahu_hoheng_fitfun/views/login.dart';
 import 'package:hahu_hoheng_fitfun/views/splash.dart';
 import 'routes/route.dart';
 
+late bool isLogin;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+  );
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    isLogin = user != null;
+  });
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(const MyApp());
 }
 
@@ -34,8 +47,8 @@ class MyApp extends StatelessWidget {
       title: 'FitFun',
       theme: AppTheme.lightTheme,
       getPages: AppPage.pages,
-      initialBinding: LoginBinding(),
-      home: const LoginView(),
+      initialBinding: isLogin ? HomeBinding() : LoginBinding(),
+      home: isLogin ? const HomeView() : const LoginView(),
     );
   }
 }
