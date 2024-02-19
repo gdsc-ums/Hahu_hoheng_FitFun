d80fe5755c7c3d3ca3594790ef6c3627229dc55f
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2024-02-19 22:07:44 +0700

    refactor the code

--- lib/views/health_score.dart
+++ lib/views/health_score.dart
@@ -12,62 +12,53 @@ class HealthScoreView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        scrolledUnderElevation: 0,
-        leading: GestureDetector(
+          backgroundColor: Colors.white,
+          scrolledUnderElevation: 0,
+          leading: GestureDetector(
             onTap: () => Get.back(),
             child: const Padding(
               padding: EdgeInsets.only(left: 10.0),
               child: Icon(Icons.arrow_back_ios),
-            )),
-        title: Text(
-          "Skor Kesehatan",
-          style: text20,
-        ),
-        centerTitle: true,
-      ),
+            ),
+          ),
+          title: Text("Skor Kesehatan", style: text20),
+          centerTitle: true),
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsetsDirectional.symmetric(horizontal: 15),
-        child: SizedBox(
-          height: Get.height,
-          width: Get.width,
-          child: Stack(
-            children: [
-              const SingleChildScrollView(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 30,
-                    ),
-                    HealthScoreHeader(),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    HealthScoreRecommendation(),
-                    SizedBox(
-                      height: 100,
-                    )
-                  ],
+        child: Padding(
+          padding: const EdgeInsetsDirectional.symmetric(horizontal: 15),
+          child: SizedBox(
+            height: Get.height,
+            width: Get.width,
+            child: Stack(
+              children: [
+                const SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 30),
+                      HealthScoreHeader(),
+                      SizedBox(height: 20),
+                      HealthScoreRecommendation(),
+                      SizedBox(height: 100)
+                    ],
+                  ),
                 ),
-              ),
-              Positioned(
+                Positioned(
                   bottom: 15,
                   child: SizedBox(
                     width: Get.width - 30,
                     child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: secondaryColor),
-                        onPressed: () {},
-                        child: Text(
-                          "Mulai Latihan",
-                          style: buttonText,
-                        )),
-                  ))
-            ],
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: secondaryColor),
+                      onPressed: () {},
+                      child: Text("Mulai Latihan", style: buttonText),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-      )),
+      ),
     );
   }
 }
--- lib/views/splash.dart
+++ lib/views/splash.dart
@@ -14,8 +14,7 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView> {
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 5), () {
-    });
+    Future.delayed(const Duration(seconds: 5), () {});
     super.initState();
   }
 
@@ -28,27 +27,25 @@ class _SplashViewState extends State<SplashView> {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SvgPicture.asset(
-                'assets/images/logo/logo_produk.svg',
-              ),
+              SvgPicture.asset('assets/images/logo/logo_produk.svg'),
             ],
           ),
-          const SizedBox(
-            height: 20,
-          ),
+          const SizedBox(height: 20),
           RichText(
             text: TextSpan(
               children: [
                 TextSpan(
-                    text: "Fit",
-                    style: text45.copyWith(
-                      color: const Color(0xFF363636),
-                    )),
+                  text: "Fit",
+                  style: text45.copyWith(
+                    color: const Color(0xFF363636),
+                  ),
+                ),
                 TextSpan(
-                    text: "fun",
-                    style: text45.copyWith(
-                      color: const Color(0xFF3CB371),
-                    )),
+                  text: "fun",
+                  style: text45.copyWith(
+                    color: const Color(0xFF3CB371),
+                  ),
+                ),
               ],
             ),
           ),
